6b22a588163dce10999468dec9602269f4bcadb9
dg1223 <alavi1223@hotmail.com>
2023-10-16 23:54:58 -0400

    pattern matching 28 - better solution

--- problem-solving/patterns_28.py
+++ problem-solving/patterns_28.py
@@ -16,31 +16,50 @@
 def pattern28(n):
     pattern = ''    
     num_rows = 2*n-1
-    col_counter = 1
-    num_spaces_decremented = 2
+    col_counter = 1    
     for row in range(num_rows):
-        # impossible case handling
-        if col_counter == 0:
-            print("This cannot happen. Revisit your logic. Exiting program...")
-            break
-        # put pen on paper to understand the concept
-        num_spaces = n - row        
-        if col_counter == 1 and num_spaces > 0:            
-            pattern += (' ' * num_spaces) + ('*' * col_counter) + (' ' * num_spaces)
-        elif col_counter != 1 and num_spaces > 0:
-            pattern += (' ' * num_spaces) + ('* ' * col_counter) + (' ' * (num_spaces-1))
-        elif col_counter != 1 and num_spaces <= 0:
-            pattern += (' ' * num_spaces_decremented) + ('* ' * col_counter) + (' ' * (num_spaces_decremented-1))
-            num_spaces_decremented += 1
-        else:
-            pattern += (' ' * num_spaces_decremented) + ('*' * col_counter) + (' ' * num_spaces_decremented)
-        
+        spaces_before = abs(col_counter - 5)
+        pattern += ' ' * spaces_before
+        for column in range(col_counter):        
+            pattern += '* '
+        pattern = pattern.rstrip()        
         pattern += '\n'
         
+        # put pen on paper to understand this
         if row >= n-1:
             col_counter -= 1
         else:
             col_counter += 1
     return pattern
+
+# def pattern28(n):
+#     pattern = ''    
+#     num_rows = 2*n-1
+#     col_counter = 1
+#     num_spaces_decremented = 2
+#     for row in range(num_rows):
+#         # impossible case handling
+#         if col_counter == 0:
+#             print("This cannot happen. Revisit your logic. Exiting program...")
+#             break
+#         # put pen on paper to understand the concept
+#         num_spaces = n - row        
+#         if col_counter == 1 and num_spaces > 0:            
+#             pattern += (' ' * num_spaces) + ('*' * col_counter) + (' ' * num_spaces)
+#         elif col_counter != 1 and num_spaces > 0:
+#             pattern += (' ' * num_spaces) + ('* ' * col_counter) + (' ' * (num_spaces-1))
+#         elif col_counter != 1 and num_spaces <= 0:
+#             pattern += (' ' * num_spaces_decremented) + ('* ' * col_counter) + (' ' * (num_spaces_decremented-1))
+#             num_spaces_decremented += 1
+#         else:
+#             pattern += (' ' * num_spaces_decremented) + ('*' * col_counter) + (' ' * num_spaces_decremented)
+        
+#         pattern += '\n'
+        
+#         if row >= n-1:
+#             col_counter -= 1
+#         else:
+#             col_counter += 1
+#     return pattern
 
 print(pattern28(5))
