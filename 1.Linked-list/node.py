71bec616c0731a4761989d7f13d1f1f73fd9d85e
dg1223 <alavi1223@hotmail.com>
2023-10-19 22:12:48 -0400

    updated Node class

--- 1.Linked-list/node.py
+++ 1.Linked-list/node.py
@@ -1,25 +1,25 @@
 class Node:
-    def __init__(self, value, link_node=None):
+    def __init__(self, value, next_node=None):
         self.value = value
-        self.link_node = link_node
-
-    def set_link_node(self, link_node):
-        self.link_node = link_node
-
-    def get_link_node(self):
-        return self.link_node
-    
+        self.next_node = next_node
+        
     def get_value(self):
         return self.value
+    
+    def get_next_node(self):
+        return self.next_node
+    
+    def set_next_node(self, next_node):
+        self.next_node = next_node
 
 instance1 = Node("some value")
 instance2 = Node("some other value")
 
-instance1.set_link_node(instance2)
+instance1.set_next_node(instance2)
 
-instance2_link_node_data = instance1.get_link_node().get_value()
+instance2_next_node_data = instance1.get_next_node().get_value()
 
 print(instance1)
 print(instance1.get_value())
-print(instance1.get_link_node)
-print(instance2_link_node_data)
+print(instance1.get_next_node)
+print(instance2_next_node_data)
