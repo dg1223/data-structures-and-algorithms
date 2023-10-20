0240c01a7e9a8f8b1445a16a0e10970d71cf4978
dg1223 <alavi1223@hotmail.com>
2023-10-19 23:33:20 -0400

    LinkedList class

--- 1.Linked-list/node.py
+++ 1.Linked-list/node.py
@@ -12,14 +12,14 @@ class Node:
     def set_next_node(self, next_node):
         self.next_node = next_node
 
-instance1 = Node("some value")
-instance2 = Node("some other value")
+# instance1 = Node("some value")
+# instance2 = Node("some other value")
 
-instance1.set_next_node(instance2)
+# instance1.set_next_node(instance2)
 
-instance2_next_node_data = instance1.get_next_node().get_value()
+# instance2_next_node_data = instance1.get_next_node().get_value()
 
-print(instance1)
-print(instance1.get_value())
-print(instance1.get_next_node)
-print(instance2_next_node_data)
+# print(instance1)
+# print(instance1.get_value())
+# print(instance1.get_next_node)
+# print(instance2_next_node_data)
