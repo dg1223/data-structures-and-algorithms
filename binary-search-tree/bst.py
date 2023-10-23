9cd0daad53bb3961c83be4ba87f8168697dab513
dg1223 <alavi1223@hotmail.com>
2023-10-23 03:35:14 -0400

    feature: BST class

---
+++ binary-search-tree/bst.py
@@ -0,0 +1,62 @@
+'''
+BST methods:
+initialize, insert, search value, dfs traversal
+
+*No need to learn  to remove a node from a BST
+as we usually use it to search for a value
+'''
+class BST:
+    def __init__(self, value, depth=0):
+        self.value = value
+        self.depth = depth
+        self.left = None
+        self.right = None
+
+    def insert(self, value):
+        if value < self.value:
+            if self.left is None:
+                self.left = BST(value, self.depth+1)
+                print(f'Tree node {value} added to the left of {self.value} at depth {self.depth + 1}')
+            else:
+                self.left.insert(value)
+        elif value > self.value:
+            if self.right is None:
+                self.right = BST(value, self.depth+1)
+                print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
+            else:
+                self.right.insert(value)
+        else:
+            self.right.insert(value)
+
+    def get_node_by_value(self, value):
+        if value == self.value:
+            return self
+        elif self.left and value < self.value:
+            self.left.get_node_by_value(value)
+        elif self.right and value >= self.value:
+            self.right.get_node_by_value(value)
+        else:
+            return None
+
+    def traverse_dfs(self):
+        if self.left:
+            self.left.traverse_dfs()
+        print(f"Depth = {self.depth}, Value = {self.value}")
+        if self.right:
+            self.right.traverse_dfs()
+
+
+
+root = BST(100)
+
+# Test 1
+tree = BST(48)
+tree.insert(24)
+tree.insert(55)
+tree.insert(26)
+tree.insert(38)
+tree.insert(56)
+tree.insert(74)
+
+# Print depth-first traversal:
+tree.traverse_dfs()
