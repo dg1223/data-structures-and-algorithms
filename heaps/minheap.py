4b4926dd73297c8fffaee80d101b9449a9a181c9
dg1223 <alavi1223@hotmail.com>
2023-10-23 23:37:10 -0400

    feature: MinHeap class

---
+++ heaps/minheap.py
@@ -0,0 +1,66 @@
+class MinHeap:
+    def __init__(self, heap_list=None):
+        # add sentinel value if not initially added 
+        if heap_list and heap_list[0] != None:
+            self.heap_list = [None]
+            self.heap_list += heap_list
+        else:
+            self.heap_list = heap_list
+
+        if not heap_list:
+            self.count = 0
+        else:
+            self.count = len(self.heap_list) - 1
+
+    def parent_index(self, index):
+        return index // 2
+
+    def left_child_index(self, index):
+        return index * 2
+
+    def right_child_index(self, index):
+        return (index * 2) + 1
+
+    def add(self, element):
+        '''
+        increment count
+        add element to heap list
+        reorder the heap
+        '''
+        self.count += 1
+        print("Adding {0} to {1}\n".format(element, self.heap_list))
+        self.heap_list.append(element)
+        self.heapify_up()
+
+    def heapify_up(self):
+        print("Heapifying up")
+        # the element that was added is the last element
+        index = self.count
+        parent_index = self.parent_index(index)
+
+        while parent_index > 0: # index 0 has None
+            parent = self.heap_list[parent_index]
+            child = self.heap_list[index]
+            if parent > child:
+                print(f"swapping {parent} with {child}")
+                self.heap_list[parent_index] = child
+                self.heap_list[index] = parent
+                print(f"current heap list: {self.heap_list}\n")
+            index = parent_index
+            parent_index = self.parent_index(index)
+            
+        print("HEAP RESTORED! {0}".format(self.heap_list))
+
+# test
+heap_list = [10, 13, 21, 22, 23, 61, 99]
+min_heap = MinHeap(heap_list)
+print(f"initial heap list: {min_heap.heap_list}")
+print(f"intial count = {min_heap.count}\n")
+
+#print("the parent index of 4 is:")
+#print(max_heap.parent_index(4))
+
+#print("the left child index of 3 is:")
+#print(max_heap.left_child_index(3))
+
+min_heap.add(5)
