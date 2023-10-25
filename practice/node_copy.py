64408ea459f2e71a17356c7eb11352b01ae997f2
dg1223 <alavi1223@hotmail.com>
2023-10-25 16:43:14 -0400

    refactor: renamed next_node to next

--- practice/node_copy.py
+++ practice/node_copy.py
@@ -1,18 +1,18 @@
 class Node:
-    def __init__(self, value, next_node=None):
+    def __init__(self, value, next=None):
         self.value = value
-        self.next_node = next_node
+        self.next = next
 
-    def set_next_node(self, next_node):
-        self.next_node = next_node
+    def set_next_node(self, next):
+        self.next = next
 
 instance1 = Node("some value")
 instance2 = Node("some other value")
 
 instance1.set_next_node(instance2)
-instance2_next_node_data = instance1.next_node.value
+instance2_next_node_data = instance1.next.value
 
 print(instance1)
 print(instance1.value)
-print(instance1.next_node)
+print(instance1.next)
 print(instance2_next_node_data)
