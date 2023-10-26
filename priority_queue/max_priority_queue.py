5fb24a5a2d11444b04f2b133db31e1da870ff32c
dg1223 <alavi1223@hotmail.com>
2023-10-26 02:40:40 -0400

    feature: min priority queue practice

--- priority_queue/max_priority_queue.py
+++ priority_queue/max_priority_queue.py
@@ -1,107 +1,93 @@
-'''
-MaxHeap + (child_present, retrieve_max, heapify_down)
-heapify_down uses get_larger_child_index
-'''
-
-class MaxHeapQ:
-    def __init__(self, heap_list=None):
-        # add sentinel value if not initially added 
-        if heap_list and heap_list[0] != None:
-            self.heap_list = [None]
-            self.heap_list += heap_list
-        else:
-            self.heap_list = heap_list
-
-        if not heap_list:
+class MaxheapQ:
+    def __init__(self, heaplist=None):
+        if not heaplist:
+            self.heaplist = []
             self.count = 0
         else:
-            self.count = len(self.heap_list) - 1
+            self.heaplist = heaplist
+            self.count = len(self.heaplist)-1
 
     def parent_index(self, index):
-        return index // 2
+        return (index - 1) // 2
 
     def left_child_index(self, index):
-        return index * 2
+        return (index * 2) + 1
 
     def right_child_index(self, index):
-        return (index * 2) + 1
+        return (index * 2) + 2
 
     def child_present(self, index):
         return self.left_child_index(index) <= self.count
 
-    def add(self, element):
-        '''
-        increment count
-        add element to heap list
-        reorder the heap
-        '''
+    def add(self, value):
         self.count += 1
-        print("Adding {0} to {1}\n".format(element, self.heap_list))
-        self.heap_list.append(element)
+        print("Adding {0} to {1}\n".format(value, self.heaplist))
+        self.heaplist.append(value)
         self.heapify_up()
 
     def heapify_up(self):
         print("Heapifying up")
-        # the element that was added is the last element
         index = self.count
         parent_index = self.parent_index(index)
 
-        while parent_index > 0: # index 0 has None
-            parent = self.heap_list[parent_index]
-            child = self.heap_list[index]
+        while parent_index >= 0:
+            parent = self.heaplist[parent_index]
+            child = self.heaplist[index]
             if parent < child:
                 print(f"swapping {parent} with {child}")
-                self.heap_list[parent_index] = child
-                self.heap_list[index] = parent
-                print(f"current heap list: {self.heap_list}\n")
-            index = parent_index
-            parent_index = self.parent_index(index)
-            
-        print("HEAP RESTORED! {0}".format(self.heap_list))
+                self.heaplist[parent_index] = child
+                self.heaplist[index] = parent
+                print(f"current heap list: {self.heaplist}\n")
+                index = parent_index
+                parent_index = self.parent_index(index)
+            # here, we are assuming that a valid heap is provided
+            # improvement -> function to check if input heap is valid
+            else:                
+                break
+        print("Restored Heap: {0}\n".format(self.heaplist))
 
     def retrieve_max(self):
         last_index = self.count
-        if last_index == 0:
-            print("No item in heap")
-            return
-        max_value = self.heap_list[1]
-        print(f">>> Maximum value = {max_value} <<<\n")
+        max_val = self.heaplist[0]
+        print(f">>> Maximum value = {max_val} <<<\n")
         print("Reordering priority queue...clear")
-        print(f"Removing: {max_value} from {self.heap_list}")
-        self.heap_list[1] = self.heap_list[last_index]
-        self.heap_list.pop()
+        print(f"Removing: {max_val} from {self.heaplist}")
+        self.heaplist[0] = self.heaplist[last_index]
+        self.heaplist.pop()
         self.count -= 1
-        print(f"{max_value} removed. Last element moved to the beginning: {self.heap_list}\n")
+        print(f"{max_val} removed. Last element moved to the beginning: {self.heaplist}\n")
         self.heapify_down()
 
-        return max_value
+        return max_val
 
     def heapify_down(self):
-        index = 1
+        index = 0
         print("Heapifying down...")
         while self.child_present(index):
-            print(f"Retrieving larger child index of {self.heap_list[index]}")
+            print(f"Retrieving larger child index of {self.heaplist[index]}")
             larger_child_index = self.get_larger_child_index(index)
-            parent = self.heap_list[index]
-            child = self.heap_list[larger_child_index]
+            parent = self.heaplist[index]
+            child = self.heaplist[larger_child_index]
             if parent < child:
                 print(f"swapping {parent} with {child}")
-                self.heap_list[index] = child
-                self.heap_list[larger_child_index] = parent
-                print(f"current heap list: {self.heap_list}\n")
-            index = larger_child_index
+                self.heaplist[index] = child
+                self.heaplist[larger_child_index] = parent
+                print(f"current heap list: {self.heaplist}\n")
+                index = larger_child_index
+            else:
+                break
         print("No more children exist")
-        print(f"\nHEAP RESTORED! {self.heap_list}")
+        print(f"\nHEAP RESTORED! {self.heaplist}")
 
     def get_larger_child_index(self, index):
         if self.right_child_index(index) > self.count:
-            print(f"There is only a left child of {self.heap_list[index]}")
+            print(f"There is only a left child of {self.heaplist[index]}")
             return self.left_child_index(index)
         else:
             left_child_index = self.left_child_index(index)
             right_child_index = self.right_child_index(index)
-            left_child = self.heap_list[left_child_index]
-            right_child = self.heap_list[right_child_index]
+            left_child = self.heaplist[left_child_index]
+            right_child = self.heaplist[right_child_index]
             if left_child > right_child:
                 print(f"Left child {left_child} is larger. Picking the left child.")
                 return left_child_index
@@ -109,10 +95,11 @@ class MaxHeapQ:
                 print(f"Left child {left_child} is not larger. Picking the right child {right_child}")
                 return right_child_index
 
+
 # test
-heap_list = [99, 22, 61, 10, 21, 13, 23]
-max_heap = MaxHeapQ(heap_list)
-print(f"initial heap list: {max_heap.heap_list}\n")
+heaplist = [99, 22, 61, 10, 21, 13, 23]
+max_heap = MaxheapQ(heaplist)
+print(f"initial heap list: {max_heap.heaplist}\n")
 
 #max_heap.add(90)
 max_heap.retrieve_max()
--- priority_queue/min_priority_queue.py
+++ priority_queue/min_priority_queue.py
@@ -1,118 +1,105 @@
-'''
-MinHeap + (child_present, retrieve_min, heapify_down)
-heapify_down uses get_smaller_child_index
-'''
-
-class MinHeapQ:
-    def __init__(self, heap_list=None):
-        # add sentinel value if not initially added 
-        if heap_list and heap_list[0] != None:
-            self.heap_list = [None]
-            self.heap_list += heap_list
-        else:
-            self.heap_list = heap_list
-
-        if not heap_list:
+class MinheapQ:
+    def __init__(self, heaplist=None):
+        if not heaplist:
+            self.heaplist = []
             self.count = 0
         else:
-            self.count = len(self.heap_list) - 1
+            self.heaplist = heaplist
+            self.count = len(self.heaplist)-1
 
     def parent_index(self, index):
-        return index // 2
+        return (index - 1) // 2
 
     def left_child_index(self, index):
-        return index * 2
+        return (index * 2) + 1
 
     def right_child_index(self, index):
-        return (index * 2) + 1
+        return (index * 2) + 2
 
     def child_present(self, index):
         return self.left_child_index(index) <= self.count
 
-    def add(self, element):
-        '''
-        increment count
-        add element to heap list
-        reorder the heap
-        '''
+    def add(self, value):
         self.count += 1
-        print("Adding {0} to {1}\n".format(element, self.heap_list))
-        self.heap_list.append(element)
+        print("Adding {0} to {1}\n".format(value, self.heaplist))
+        self.heaplist.append(value)
         self.heapify_up()
 
     def heapify_up(self):
         print("Heapifying up")
-        # the element that was added is the last element
         index = self.count
         parent_index = self.parent_index(index)
 
-        while parent_index > 0: # index 0 has None
-            parent = self.heap_list[parent_index]
-            child = self.heap_list[index]
+        while parent_index >= 0:
+            parent = self.heaplist[parent_index]
+            child = self.heaplist[index]
             if parent > child:
                 print(f"swapping {parent} with {child}")
-                self.heap_list[parent_index] = child
-                self.heap_list[index] = parent
-                print(f"current heap list: {self.heap_list}\n")
-            index = parent_index
-            parent_index = self.parent_index(index)
-            
-        print("HEAP RESTORED! {0}".format(self.heap_list))
+                self.heaplist[parent_index] = child
+                self.heaplist[index] = parent
+                print(f"current heap list: {self.heaplist}\n")
+                index = parent_index
+                parent_index = self.parent_index(index)
+            # here, we are assuming that a valid heap is provided
+            # improvement -> function to check if input heap is valid
+            else:                
+                break
+        print("Restored Heap: {0}\n".format(self.heaplist))
 
     def retrieve_min(self):
         last_index = self.count
-        if last_index == 0:
-            print("No item in heap")
-            return
-        min_value = self.heap_list[1]
-        print(f">>> Minimum value = {min_value} <<<\n")
-        print("Reordering priority queue...")
-        print(f"Removing: {min_value} from {self.heap_list}")
-        self.heap_list[1] = self.heap_list[last_index]
-        self.heap_list.pop()
+        min_val = self.heaplist[0]
+        print(f">>> Minimum value = {min_val} <<<\n")
+        print("Reordering priority queue...clear")
+        print(f"Removing: {min_val} from {self.heaplist}")
+        self.heaplist[0] = self.heaplist[last_index]
+        self.heaplist.pop()
         self.count -= 1
-        print(f"{min_value} removed. Last element moved to the beginning: {self.heap_list}\n")
+        print(f"{min_val} removed. Last element moved to the beginning: {self.heaplist}\n")
         self.heapify_down()
 
-        return min_value
+        return min_val
 
     def heapify_down(self):
-        index = 1
+        index = 0
         print("Heapifying down...")
         while self.child_present(index):
-            print(f"Retrieving smaller child index of {self.heap_list[index]}")
+            print(f"Retrieving smaller child index of {self.heaplist[index]}")
             smaller_child_index = self.get_smaller_child_index(index)
-            parent = self.heap_list[index]
-            child = self.heap_list[smaller_child_index]
+            parent = self.heaplist[index]
+            child = self.heaplist[smaller_child_index]
             if parent > child:
                 print(f"swapping {parent} with {child}")
-                self.heap_list[index] = child
-                self.heap_list[smaller_child_index] = parent
-                print(f"current heap list: {self.heap_list}\n")
-            index = smaller_child_index
+                self.heaplist[index] = child
+                self.heaplist[smaller_child_index] = parent
+                print(f"current heap list: {self.heaplist}\n")
+                index = smaller_child_index
+            else:
+                break
         print("No more children exist")
-        print(f"\nHEAP RESTORED! {self.heap_list}")
+        print(f"\nHEAP RESTORED! {self.heaplist}")
 
     def get_smaller_child_index(self, index):
         if self.right_child_index(index) > self.count:
-            print(f"There is only a left child of {self.heap_list[index]}")
+            print(f"There is only a left child of {self.heaplist[index]}")
             return self.left_child_index(index)
         else:
             left_child_index = self.left_child_index(index)
             right_child_index = self.right_child_index(index)
-            left_child = self.heap_list[left_child_index]
-            right_child = self.heap_list[right_child_index]
-            if left_child < right_child:
-                print(f"Left child {left_child} is smaller. Picking the left child.")
-                return left_child_index
-            else:
-                print(f"Left child {left_child} is not smaller. Picking the right child {right_child}")
+            left_child = self.heaplist[left_child_index]
+            right_child = self.heaplist[right_child_index]
+            if right_child < left_child:
+                print(f"Right child {right_child} is smaller. Picking the right child.")
                 return right_child_index
+            else:
+                print(f"Right child {right_child} is not smaller. Picking the left child {left_child}")
+                return left_child_index
+
 
 # test
-heap_list = [10, 13, 21, 22, 23, 61, 99]
-min_heap = MinHeapQ(heap_list)
-print(f"initial heap list: {min_heap.heap_list}\n")
+heaplist = [10, 13, 21, 22, 23, 61, 99]
+max_heap = MinheapQ(heaplist)
+print(f"initial heap list: {max_heap.heaplist}\n")
 
-#min_heap.add(5)
-min_heap.retrieve_min()
+#max_heap.add(90)
+max_heap.retrieve_min()
