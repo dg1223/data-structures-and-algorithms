1985fd565e5331158d3b7b0b856a57d1f75a82ee
dg1223 <alavi1223@hotmail.com>
2023-10-26 23:48:01 -0400

    features: cyclic and acylic graphs practice

---
+++ practice/graphs/acyclic_copy.py
@@ -0,0 +1,29 @@
+from vertex_copy import Vertex
+from graph_copy import Graph
+
+no_path_exists = True
+
+directed_railway = Graph(True)
+
+callan_station = Vertex("callan")
+peel_station = Vertex("peel")
+ulfstead_station = Vertex("ulfstead")
+harwick_station = Vertex('harwick')
+
+directed_railway.add_vertex(callan_station)
+directed_railway.add_vertex(peel_station)
+directed_railway.add_vertex(harwick_station)
+directed_railway.add_vertex(ulfstead_station)
+
+directed_railway.add_edge(harwick_station, peel_station)
+directed_railway.add_edge(peel_station, callan_station)
+
+path_exists = directed_railway.find_path("harwick", "harwick")
+print(path_exists)
+
+print("\n\n\nFinding path from harwick to callan\n")
+new_path_exists = directed_railway.find_path('harwick', 'callan')
+print(new_path_exists)
+print("\n\nTrying to find path from harwick to ulfstead\n")
+no_path_exists = directed_railway.find_path('harwick', 'ulfstead')
+print(no_path_exists)
--- practice/graphs/graph_copy.py
+++ practice/graphs/graph_copy.py
@@ -11,19 +11,18 @@ class Graph:
         self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
         if not self.directed:
             self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
-
+        
     def find_path(self, start_vertex, end_vertex):
         start = [start_vertex]
         seen = {}
         while start:
             current_vertex = start.pop(0)
-            seen[current_vertex] = True
-            print("Visiting " + current_vertex)
             if current_vertex == end_vertex:
                 return True
             else:
-                vertex = self.graph_dict[current_vertex]
-                next_vertices = vertex.get_edges()
+                print("Visiting " + current_vertex)
+                seen[current_vertex] = True            
+                next_vertices = self.graph_dict[current_vertex].get_edges()
                 next_vertices = [vertex for vertex in next_vertices if vertex not in seen]
                 start += next_vertices
         return False
