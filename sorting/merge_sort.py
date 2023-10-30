b155a28bfc70530269673537ef495778c1a91c72
dg1223 <alavi1223@hotmail.com>
2023-10-30 16:02:13 -0400

    merge sort

---
+++ sorting/merge_sort.py
@@ -0,0 +1,43 @@
+def merge_sort(items):
+    length = len(items)
+    if length <= 1:
+        return items
+    mid_index = length // 2
+    left_split = items[:mid_index]
+    right_split = items[mid_index:]
+    left_sorted = merge_sort(left_split)
+    right_sorted = merge_sort(right_split)
+
+    return merge(left_sorted, right_sorted)
+
+def merge(left, right):
+    result = []
+    while left and right:
+        left_item = left[0]
+        right_item = right[0]
+        if left_item < right_item:
+            result.append(left_item)
+            left.pop(0)
+        else:
+            result.append(right_item)
+            right.pop(0)
+
+    if left:
+        result += left
+    if right:
+        result += right
+
+    return result
+
+
+unordered_list1 = [356, 746, 264, 569, 949, 895, 125, 455]
+unordered_list2 = [787, 677, 391, 318, 543, 717, 180, 113, 795, 19, 202, 534, 201, 370, 276, 975, 403, 624, 770, 595, 571, 268, 373]
+unordered_list3 = [860, 380, 151, 585, 743, 542, 147, 820, 439, 865, 924, 387]
+
+ordered_list1 = merge_sort(unordered_list1)
+ordered_list2 = merge_sort(unordered_list2)
+ordered_list3 = merge_sort(unordered_list3)
+
+print("ordered_list1\n{0}\n".format(ordered_list1))
+print("ordered_list2\n{0}\n".format(ordered_list2))
+print("ordered_list3\n{0}\n".format(ordered_list3))
