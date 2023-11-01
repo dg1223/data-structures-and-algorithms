d912f48c23c9de82b7f0111d1b2330939a254e85
dg1223 <alavi1223@hotmail.com>
2023-11-01 00:49:18 -0400

    debug: indentation

--- binary_search/breadth_first_search/treenode.py
+++ binary_search/breadth_first_search/treenode.py
@@ -9,13 +9,15 @@ class Treenode:
         stack.append([self, 0])
         level_str = "\n"
         while len(stack) > 0:
-        node, level = stack.pop()
+            node, level = stack.pop()
         
         if level > 0:
             level_str += "| "*(level-1)+ "|-"
+
         level_str += str(node.value)
         level_str += "\n"
         level+=1
+
         for child in reversed(node.children):
             stack.append([child, level])
 
