7352eeec042b08998e7d84d81887491d20fc6e8d
dg1223 <alavi1223@hotmail.com>
2023-11-02 18:03:56 -0400

    feature: recursive DFS graph

---
+++ graphs/recursion/dfs.py
@@ -0,0 +1,25 @@
+def dfs(graph, start_vertex, target_vertex, visited=None):
+    if not visited:
+        visited = []
+
+    visited = visited + [start_vertex]
+    
+    if start_vertex == target_vertex:
+        return visited
+    
+    for neighbour in graph[start_vertex]:
+        if neighbour not in visited:
+            path = dfs(graph, neighbour, target_vertex, visited)
+            if path:
+                return path
+
+the_most_dangerous_graph = {
+  'lava': set(['sharks', 'piranhas']),
+  'sharks': set(['lava', 'bees', 'lasers']),
+  'piranhas': set(['lava', 'crocodiles']),
+  'bees': set(['sharks']),
+  'lasers': set(['sharks', 'crocodiles']),
+  'crocodiles': set(['piranhas', 'lasers'])
+  }
+
+print(dfs(the_most_dangerous_graph, "crocodiles", "bees"))
