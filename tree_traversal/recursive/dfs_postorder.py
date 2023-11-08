f912360bfbf9c8cf85a61ea4333337a67a1f6c65
dg1223 <alavi1223@hotmail.com>
2023-11-07 22:30:04 -0500

    feature: DFS recursive tree traversal, post order

---
+++ tree_traversal/recursive/dfs_postorder.py
@@ -0,0 +1,42 @@
+def recursive_dfs(structure, stack, visited):
+    if not stack:
+        return None
+
+    current_node = stack.pop()    
+
+    if current_node in structure:
+        for neighbour in structure[current_node]:
+            if neighbour not in visited:
+                stack.append(neighbour)
+                visited.add(neighbour)
+                # call DFS to traverse the children before 
+                # it reaches the sibling
+                recursive_dfs(structure, stack, visited)
+    
+    print(current_node)
+
+def dfs(structure, start):
+    visited = set()
+    stack = [start]
+    visited.add(start)
+    recursive_dfs(structure, stack, visited)
+
+## Create a graph represented as an adjacency list
+#graph = {
+#    'A': ['B', 'C'],
+#    'B': ['A', 'D', 'E'],
+#    'C': ['A', 'F'],
+#    'D': ['B'],
+#    'E': ['B', 'F'],
+#    'F': ['C', 'E']
+#}
+#dfs(graph, 'A')
+
+tree = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F', 'G']
+}
+
+# Call BFS starting from node 'A'
+dfs(tree, 'A')
