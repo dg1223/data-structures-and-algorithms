d7ed13423561fbdd9132956c62b194cb8ca88184
dg1223 <alavi1223@hotmail.com>
2023-11-10 13:38:01 -0500

    feature: kth smallest by sorting O(NlogN)

--- problem-solving/kth_smallest_largest/kth_smallest_sorting.py
+++ problem-solving/kth_smallest_largest/kth_smallest_sorting.py
@@ -1,14 +1,13 @@
 def sort_array(array):
-    # perform selection sort: O(N^2)
-    # or use the sort() function for O(NlogN)
-    length = len(array)
-    for idx_1 in range(length):
-        for idx_2 in range(idx_1+1, length):
-            if array[idx_1] > array[idx_2]:
-                array[idx_1], array[idx_2] = array[idx_2], array[idx_1]
+    array.sort()
+    ## selection sort: O(N^2)
+    #length = len(array)
+    #for idx_1 in range(length):
+    #    for idx_2 in range(idx_1+1, length):
+    #        if array[idx_1] > array[idx_2]:
+    #            array[idx_1], array[idx_2] = array[idx_2], array[idx_1]
 
 def kth_smallest(array, k):
-    # sort array
     sort_array(array)
     return array[k-1]
 
