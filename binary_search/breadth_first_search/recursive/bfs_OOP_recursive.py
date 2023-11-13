a18404ce0dcab929792e0b9bd2967052e362e429
dg1223 <alavi1223@hotmail.com>
2023-11-13 18:47:19 -0500

    features: BFS iterative and recursive using OOP

---
+++ binary_search/breadth_first_search/recursive/bfs_OOP_recursive.py
@@ -0,0 +1,52 @@
+from collections import deque
+
+class BFS:
+	def __init__(self, value):
+		self.value = value
+		self.children = []
+
+	def bfs_recursive(self, frontier, target):
+		if not frontier:
+			return None
+
+		current_path = frontier.pop()
+		current_node = current_path[-1]
+
+		if current_node.value == target:
+			return current_path
+
+		for child in current_node.children:
+			new_path = current_path.copy()
+			new_path.append(child)
+			frontier.appendleft(new_path)
+
+		return self.bfs_recursive(frontier, target)
+
+	def bfs(self, root, target):
+		frontier = deque()
+		start = [root]
+		frontier.appendleft(start)
+
+		return self.bfs_recursive(frontier, target)
+
+sample_root_node = BFS("Home")
+docs = BFS("Documents")
+photos = BFS("Photos")
+
+sample_root_node.children = [docs, photos]
+
+my_wish = BFS("WishList.txt")
+my_todo = BFS("TodoList.txt")
+my_cat = BFS("Fluffy.jpg")
+my_dog = BFS("Spot.jpg")
+
+docs.children = [my_wish, my_todo]
+photos.children = [my_cat, my_dog]
+
+path_to_target = sample_root_node.bfs(sample_root_node, "Fluffy.jpg")
+if not path_to_target:
+    print("No path found")
+else:
+    print("Path found!")
+    for node in path_to_target:
+        print(node.value)
