59c56a56d3cb31120dcce1a9acdc7068859f2a23
dg1223 <alavi1223@hotmail.com>
2023-11-18 00:00:17 -0500

    debug: fixed typo

--- linked_list/node.py
+++ linked_list/node.py
@@ -1,7 +1,7 @@
 class Node:
     def __init__(self, value, next_node=None):
         self.value = value
-        self.next_node = next
+        self.next_node = next_node
 
     def set_next_node(self, next_node):
         self.next_node = next_node
