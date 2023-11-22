49dfa1ef58860a289af3f4677abb932e20f15da2
dg1223 <alavi1223@hotmail.com>
2023-11-22 02:06:25 -0500

    chores: added stack tracing visualization

--- tree_traversal/recursive/DFS/dfs_inorder.py
+++ tree_traversal/recursive/DFS/dfs_inorder.py
@@ -1,14 +1,15 @@
-def recursive_inorder_dfs(graph, current_vertex):
-    if current_vertex in graph:
-        left_neighbour, right_neighbour = graph[current_vertex]
-        if left_neighbour:
-            recursive_inorder_dfs(graph, left_neighbour)
-        print(current_vertex)
-        if right_neighbour:
-            recursive_inorder_dfs(graph, right_neighbour)
+def inorder_dfs(graph, current_vertex):
+	# Edge case: input tree is empty
+	if current_vertex in graph:
+		left_neighbour, right_neighbour = graph[current_vertex]
+		if left_neighbour:
+			inorder_dfs(graph, left_neighbour)
+		print(current_vertex)
+		if right_neighbour:
+			inorder_dfs(graph, right_neighbour)
 
-def inorder_dfs(graph, start):
-    recursive_inorder_dfs(graph, start)
+def dfs(graph, start):
+	inorder_dfs(graph, start)
 
 tree = {
     'B': ['D', 'E'],
@@ -20,5 +21,38 @@ tree = {
     'G': [None, None]
 }
 
+#tree = {}
+
 # Call in-order DFS starting from node 'A'
-inorder_dfs(tree, 'A')
+dfs(tree, 'A')
+
+'''
+
+		A
+	  /   \
+	 B     C
+    / \   / \
+   D   E F   G
+
+
+|                 |			   |                 |			   |                 |			   |                 |		|                 |
+|-----------------|			   |-----------------|			   |-----------------|			   |-----------------|		|-----------------|
+| dfs(graph, 'D') |-> print: D | 	  pop(D)	 |			   | dfs(graph, 'E') |-> print: E  | 	  pop(E)	 |		|				  |
+|-----------------|			   |-----------------|			   |-----------------|			   |-----------------| -> 	|-----------------|
+| dfs(graph, 'B') |			   | dfs(graph, 'B') |-> print: B  | dfs(graph, 'B') |			   | dfs(graph, 'B') |		| 	   pop(B)	  |
+|-----------------|			   |-----------------|			   |-----------------|			   |-----------------|		|-----------------| -> print: A
+| dfs(graph, 'A') |			   | dfs(graph, 'A') |			   | dfs(graph, 'A') |			   | dfs(graph, 'A') |		| dfs(graph, 'A') |
+-------------------			   -------------------			   -------------------			   -------------------		-------------------
+
+|                 |			   |                 |			   |                 |			   |                 |				|                 |
+|-----------------|			   |-----------------|			   |-----------------|			   |-----------------|				|-----------------|
+| 				  |-> 		   | dfs(graph, 'F') |-> print: F  |      pop(F)	 |  		   | dfs(graph, 'G') |-> print: G	|	   pop(G)	  |
+|-----------------|			   |-----------------|			   |-----------------|			   |-----------------|				|-----------------|
+| dfs(graph, 'C') |			   | dfs(graph, 'C') |			   | dfs(graph, 'C') |-> print: C  | dfs(graph, 'C') |				| 	   pop(C)	  |
+|-----------------|			   |-----------------|			   |-----------------|			   |-----------------|				|-----------------|
+| dfs(graph, 'A') |			   | dfs(graph, 'A') |			   | dfs(graph, 'A') |			   | dfs(graph, 'A') |				| 	   pop(A)	  |
+-------------------			   -------------------			   -------------------			   -------------------				-------------------
+
+D, B, E, A, F, C, G
+
+'''
