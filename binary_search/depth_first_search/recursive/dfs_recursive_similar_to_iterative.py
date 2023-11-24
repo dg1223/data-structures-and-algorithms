7a9870bd13f7e2fb87493df2df1ab4248fb15c03
dg1223 <alavi1223@hotmail.com>
2023-11-24 17:50:24 -0500

    feature: DFS recursive binary search alternative approach, similar to iterative

---
+++ binary_search/depth_first_search/recursive/dfs_recursive_similar_to_iterative.py
@@ -0,0 +1,45 @@
+class DFS:
+	def __init__(self, value):
+		self.value = value
+		self.children = []
+
+	def dfs(self, start_node, target):
+		path = [start_node]
+		if start_node.value == target:
+			return path
+
+		frontier = [(start_node, path)]
+		return self.dfs_recursive(frontier, target)
+
+	def dfs_recursive(self, stack, target):
+		current_node, path = stack.pop()
+		print(f"Visiting {current_node.value}")
+		if current_node.value == target:
+			return path
+		for child in current_node.children:
+			stack.append((child, path + [child]))
+			found_path = self.dfs_recursive(stack, target)
+			if found_path:
+				return found_path
+
+root = DFS('A')
+two = DFS("B")
+three = DFS("C")
+root.children = [two, three]
+four = DFS("D")
+five = DFS("E")
+six = DFS("F")
+seven = DFS("G")
+two.children = [five, four]
+three.children = [seven, six]
+
+start = root
+target = 'F'
+path = root.dfs(start, target)
+
+if path:
+	print(f"Path found from {start.value} to {target}")
+	string = " -> ".join(node.value for node in path)
+	print(string)
+else:
+	print(f"Path not found from {start.value} to {target}")
