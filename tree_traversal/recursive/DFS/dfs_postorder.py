9f7beb1b693681410c1fd0ea9a0e541de4149af5
dg1223 <alavi1223@hotmail.com>
2023-11-24 23:49:24 -0500

    refactor: renamed parameters from graph to tree

--- tree_traversal/recursive/DFS/dfs_postorder.py
+++ tree_traversal/recursive/DFS/dfs_postorder.py
@@ -1,29 +1,19 @@
 # post-order: LRP (left, right, parent)
-def postorder_dfs(graph, start_vertex, visited=None):
+def postorder_dfs(tree, start_vertex, visited=None):
 	if not visited:
 		visited = []
 
 	visited  = visited + [start_vertex]
 
-	if start_vertex in graph:
-		for neighbour in graph[start_vertex]:
+	if start_vertex in tree:
+		for neighbour in tree[start_vertex]:
 			if neighbour not in visited:
-				new_path = postorder_dfs(graph, neighbour, visited)
+				new_path = postorder_dfs(tree, neighbour, visited)
 				if new_path:
 					return new_path
 
 	print(start_vertex)
 
-## Create a graph represented as an adjacency list
-#graph = {
-#    'A': ['B', 'C'],
-#    'B': ['A', 'D', 'E'],
-#    'C': ['A', 'F'],
-#    'D': ['B'],
-#    'E': ['B', 'F'],
-#    'F': ['C', 'E']
-#}
-#postorder_dfs(graph, 'A')
 
 tree = {
     'A': ['B', 'C'],
