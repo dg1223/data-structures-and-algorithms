55ec12edeb620e8a92230a5063da6e94c4aaf6bd
dg1223 <alavi1223@hotmail.com>
2023-11-24 23:14:04 -0500

    refactor: rearranged code

--- tree_traversal/recursive/bfs.py
+++ tree_traversal/recursive/bfs.py
@@ -1,5 +1,23 @@
 from collections import deque
 
+def bfs(graph, start):
+    visited = set()
+    '''
+    For tree traversal algos, we initialize the tree as 
+    a graph using adjacency lists. We also don't need to 
+    keep track of any path since we are merely traversing  
+    one vertex after another. So, we only need a 1-D list
+    /array as we don't need to hold additional lists.
+    On the contrary, for binary search, we need to keep 
+    track of each path separately. Each path also contains 
+    one or more vertices. So, we need a 2-D list that 
+    can hold multiple lists as paths.	
+    '''
+    queue = deque()
+    queue.append(start)
+    visited.add(start)
+    recursive_bfs(graph, queue, visited)
+
 def recursive_bfs(graph, queue, visited):
     if not queue:
         return None
@@ -20,23 +38,6 @@ def recursive_bfs(graph, queue, visited):
 
     recursive_bfs(graph, queue, visited)
 
-def bfs(graph, start):
-    visited = set()
-    '''
-    For tree traversal algos, we initialize the tree as 
-    a graph using adjacency lists. We also don't need to 
-    keep track of any path since we are merely traversing  
-    one vertex after another. So, we only need a 1-D list
-    /array as we don't need to hold additional lists.
-    On the contrary, for binary search, we need to keep 
-    track of each path separately. Each path also contains 
-    one or more vertices. So, we need a 2-D list that 
-    can hold multiple lists as paths.	
-    '''
-    queue = deque()
-    queue.append(start)
-    visited.add(start)
-    recursive_bfs(graph, queue, visited)
 
 # Create a graph represented as an adjacency list
 graph = {
