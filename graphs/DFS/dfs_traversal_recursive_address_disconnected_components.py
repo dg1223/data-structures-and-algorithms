66f96da74e4cbaf1ebba555aeb14a2fb0fbda317
dg1223 <alavi1223@hotmail.com>
2024-02-05 00:24:36 -0500

    feature: DFS traversal on disconnected components

---
+++ graphs/DFS/dfs_traversal_recursive_address_disconnected_components.py
@@ -0,0 +1,66 @@
+'''
+O(V+E) time, O(V) space
+'''
+
+visited = set()
+
+def dfs(graph, source):
+
+    print(source)
+
+    visited.add(source)
+
+    for child in graph[source]:
+        if child not in visited:
+            dfs(graph, child)
+
+'''
+			A
+		/	  	\
+	   B     	 C
+	 / 	 \     /
+	D     E - F
+'''
+
+graph_1 = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+'''
+The for loop takes care disconnected components
+'''
+for vertex in graph_1:
+	if vertex not in visited:
+		dfs(graph_1, vertex)
+
+print()
+
+'''
+		 0	    3
+		/	 / 	   \
+	   1    4 	    6
+	  /	     \     /
+	 2          5
+'''
+
+graph_2 = {
+     '0': ['1'],
+     '1': ['0', '2'],
+	 '2': ['1'],
+     '3': ['4', '6'],
+     '4': ['3', '5'],
+     '5': ['4', '6'],
+     '6': ['3', '5']
+}
+
+'''
+The for loop takes care disconnected components
+'''
+for vertex in graph_2:
+	if vertex not in visited:
+		dfs(graph_2, vertex)
