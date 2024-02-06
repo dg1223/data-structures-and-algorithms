baec842c3c601aeb5b2e872b9daaf10a94f343a0
dg1223 <alavi1223@hotmail.com>
2024-02-05 21:33:21 -0500

    feature: count connected components DFS

---
+++ graphs/DFS/dfs_count_connected_components.py
@@ -0,0 +1,107 @@
+'''
+O(V+E) time, O(V) space
+
+Is a single node considered connected?
+No. A 1-component is just an ordinary component.
+'''
+
+def dfs(graph, source):
+    visited.add(source)
+
+    if not graph[source]:
+        node_is_single = True
+
+    for child in graph[source]:
+        if child not in visited:
+            dfs(graph, child)
+
+def count_components(graph, source, visited):
+    num_components = 0
+    '''
+    We are going to perform DFS on every vertex.
+    If there's a dead-end, the function will return.
+    Then we increment the number of components by 1.
+    For every component, dfs() will return once. This
+    is how we can count the number of components.
+    '''
+    for vertex in graph:
+        if vertex not in visited:
+            dfs(graph, vertex)
+            num_components += 1
+
+    return num_components
+
+
+'''
+            A
+        /	  	\
+       B     	 C
+     / 	 \     /
+    D     E - F
+'''
+
+graph_1 = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D', 'E'],
+    'C': ['A', 'F'],
+    'D': ['B'],
+    'E': ['B', 'F'],
+    'F': ['C', 'E']
+}
+
+visited = set()
+node_is_single = False
+result = count_components(graph_1, 'A', visited)
+connected_components = result if not node_is_single else result-1
+print(f"{connected_components = }")
+
+
+'''
+         0	    3
+        /	 / 	   \
+       1    4 	    6
+      /	     \     /
+     2          5
+'''
+
+graph_2 = {
+     '0': ['1'],
+     '1': ['0', '2'],
+     '2': ['1'],
+     '3': ['4', '6'],
+     '4': ['3', '5'],
+     '5': ['4', '6'],
+     '6': ['3', '5']
+}
+
+visited = set()
+node_is_single = False
+result = count_components(graph_2, '0', visited)
+connected_components = result if not node_is_single else result-1
+print(f"{connected_components = }")
+
+
+'''
+         0	    3		7
+        /	 / 	   \
+       1    4 	    6
+      /	     \     /
+     2          5
+'''
+
+graph_3 = {
+     '0': ['1'],
+     '1': ['0', '2'],
+     '2': ['1'],
+     '3': ['4', '6'],
+     '4': ['3', '5'],
+     '5': ['4', '6'],
+     '6': ['3', '5'],
+     '7': []
+}
+
+visited = set()
+node_is_single = False
+result = count_components(graph_2, '0', visited)
+connected_components = result if not node_is_single else result-1
+print(f"{connected_components = }")
